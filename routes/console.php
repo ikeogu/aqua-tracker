f552d3e7b6126de931572c8dbfce2005916a7612
ikeogu <ikeogu322@gmail.com>
2024-04-04 11:49:47 +0100

    comleted major functions

--- routes/console.php
+++ routes/console.php
@@ -10,7 +10,7 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote')->hourly();
 
 
-Schedule::command('app:update-task-status-command')->addMinute();
+Schedule::command('app:update-task-status-command')->everyMinute();
 
 
 
