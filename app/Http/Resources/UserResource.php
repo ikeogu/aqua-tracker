db5a9e32ceb38531ad733044b83434a0aeb55402
ikeogu <ikeogu322@gmail.com>
2024-04-10 10:02:41 +0100

    rework

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Enums\Role;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,7 +15,7 @@ class UserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        
+
         return [
             'id' => $this->id,
             'type' => 'user',
@@ -29,27 +30,53 @@ class UserResource extends JsonResource
             ],
 
 
-            'organizations' => $this->whenLoaded('tenants',function () {
-                return $this->tenants->map(function ($tenant) {
-                    return [
-                        'id' => $tenant->id,
-                        'type' => 'tenant',
-                        'attributes' => [
 
-                            'organization_name' => $tenant->organization_name,
-                            'no_of_farms_owned' => $tenant->no_of_farms_owned,
-                            'capital' => $tenant->capital
-                        ],
-                        'farms' => $tenant->farms->map(function ($farm) {
-                            return [
-                                'id' => $farm->id,
-                                'name' => $farm->name,
-                            ];
-                        }),
-                    ];
-                });
+            'organizations' => ($this->role === Role::ORGANIZATION_OWNER->value) ? [$this->farmOwner()] : $this->others()
+        ];
+    }
 
+    public function farmOwner() : array
+    {
+        return [
+            'id' => $this->tenant->id,
+            'type' => 'tenant',
+            'attributes' => [
+
+                'organization_name' => $this->tenant->organization_name,
+                'no_of_farms_owned' => $this->tenant->no_of_farms_owned,
+                'capital' => $this->tenant->capital
+            ],
+            'farms' => $this->tenant->farms->map(function ($farm) {
+                return [
+                    'id' => $farm->id,
+                    'name' => $farm->name,
+                ];
             }),
         ];
+    }
+
+    public function others() : mixed
+    {
+        $this->whenLoaded('tenants',function () {
+            return $this->tenants->map(function ($tenant) {
+                return [
+                    'id' => $tenant->id,
+                    'type' => 'tenant',
+                    'attributes' => [
+
+                        'organization_name' => $tenant->organization_name,
+                        'no_of_farms_owned' => $tenant->no_of_farms_owned,
+                        'capital' => $tenant->capital
+                    ],
+                    'farms' => $tenant->farms->map(function ($farm) {
+                        return [
+                            'id' => $farm->id,
+                            'name' => $farm->name,
+                        ];
+                    }),
+                ];
+            });
+
+        });
     }
 }
