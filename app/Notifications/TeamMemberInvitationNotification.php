a2ee56cefa069605660b53775368ce28f7dc2306
ikeogu <ikeogu322@gmail.com>
2024-04-19 19:25:49 +0100

    formated purchase resource

--- app/Notifications/TeamMemberInvitationNotification.php
+++ app/Notifications/TeamMemberInvitationNotification.php
@@ -32,7 +32,7 @@ class TeamMemberInvitationNotification extends Notification
      */
     public function via(object $notifiable): array
     {
-        return ['mail', 'database'];
+        return ['database'];
     }
 
     /**
