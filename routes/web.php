a1ce490c73d4ebc34ca096cf109929b00503b125
ikeogu <ikeogu322@gmail.com>
2024-05-14 09:51:44 +0100

    resolved server erro

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Enums\Role;
+use App\Models\SubscriptionPlan;
 use App\Models\User;
 use App\Services\PaymentService;
 use Illuminate\Support\Facades\Route;
@@ -13,6 +14,35 @@ Route::get('logs', [\Rap2hpoutre\LaravelLogViewer\LogViewerController::class, 'i
 
 
 Route::get('/subscribe-users', function () {
+
+
+    SubscriptionPlan::updateOrCreate(
+        ['title' => 'Basic Plan'],
+        [
+            'description' => 'Just basic plan',
+            'monthly_price' => 0,
+            'duration' => 3,
+            "type" => 'free',
+            "discount" => 0,
+            "limited_to" => [
+                'just 90 days'
+            ]
+        ]
+    );
+
+    SubscriptionPlan::updateOrCreate(
+        ['title' => 'Premuium Plan'],
+        [
+            'description' => 'All the feature',
+            'monthly_price' => 3500,
+            'duration' => 1,
+            "type" => 'premium',
+            "discount" => 20,
+            "limited_to" => [
+                'unlimited'
+            ]
+        ]
+    );
 
     /** @var User $users */
     $users = User::whereRelation('roles', 'name',  Role::ORGANIZATION_OWNER->value)->get();
