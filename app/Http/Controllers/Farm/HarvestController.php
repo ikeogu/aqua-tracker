1e21f31912c6c7789928e09c572d8af12ea4c09a
ikeogu <ikeogu322@gmail.com>
2024-05-16 12:48:19 +0100

    worked on harvest

--- app/Http/Controllers/Farm/HarvestController.php
+++ app/Http/Controllers/Farm/HarvestController.php
@@ -9,6 +9,7 @@ use App\Http\Requests\createHarvestRequest;
 use App\Http\Requests\UpdateHarvestRequest;
 use App\Http\Resources\HarvestResource;
 use App\Models\Batch;
+use App\Models\Expense;
 use App\Models\Farm;
 use App\Models\Harvest;
 use App\Models\Inventory;
@@ -16,11 +17,12 @@ use App\Models\Purchase;
 use Illuminate\Database\Query\Builder;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 
 class HarvestController extends Controller
 {
     //
-    public function index(Request $request, Farm $farm) : JsonResponse
+    public function index(Request $request, Farm $farm): JsonResponse
     {
         $harvests = $farm->harvests()->when($request->search, function ($query) use ($request) {
             return $query->where('name', 'like', '%' . $request->search . '%')
@@ -29,36 +31,54 @@ class HarvestController extends Controller
                     $query->where('name', 'like', '%' . $request->search . '%');
                 });
         })
-        ->paginate($request->per_page ?? 20)
-        ->through(function ($harvest) {
-            return [
-                'id' => $harvest->id,
-                'date' => $harvest->created_at->format('d/m/Y'),
-                'name' => $harvest->name,
-                'consultant' => $harvest->consultant,
-                'batch' => [
-                    'id' => $harvest->batch_id,
-                    'name' => $harvest->batch->name,
-                ],
-                'total_sales' => number_format($harvest->purchases()->sum('amount'),2),
+            ->paginate($request->per_page ?? 20)
+            ->through(function ($harvest) {
+                return [
+                    'id' => $harvest->id,
+                    'date' => $harvest->created_at->format('d/m/Y'),
+                    'name' => $harvest->name,
+                    'consultant' => $harvest->consultant,
+                    'batch' => [
+                        'id' => $harvest->batch_id,
+                        'name' => $harvest->batch->name,
+                    ],
+                    'total_sales' => number_format($harvest->purchases()->sum('amount'), 2),
 
-            ];
-        });
+                ];
+            });
 
 
         $totalHarvest = Purchase::whereIn('harvest_id', $farm->harvests()->pluck('harvests.id')->toArray())->sum('amount');
-        $batch_ids = $farm->harvests()->pluck('batch_id')->toArray();
+        $batch_ids = $farm->harvests()->pluck('batch_id')->unique()->toArray();
 
         $inventories = $farm->inventories()->whereIn('batch_id', $batch_ids)->sum('amount');
-        $expenses = $farm->expenses()->whereIn('splitted_for_batch->batch_id', $batch_ids)->sum('splitted_for_batch->amount');
+
+        $expenses = [];
+
+        foreach ($batch_ids as $batch) {
+            $expenses[] = Expense::where('farm_id', $farm->id)
+                ->whereJsonContains('splitted_for_batch', ['batch_id' => $batch])
+                ->get()
+                ->pluck('splitted_for_batch')
+                ->flatten()
+                ->toArray();
+        }
+        $expenses = Arr::flatten($expenses);
+
+        $totalExpenses = array_sum(array_filter($expenses, function ($item) {
+            return is_numeric($item);
+        }));
+
 
         $amount_spent = Batch::whereId($batch_ids)->sum('amount_spent');
 
         $data = [
-            'total_harvest' => $totalHarvest,
-            'total_capital' => $inventories + $expenses + $amount_spent,
-             'total_profit' =>  $totalHarvest - ($inventories + $expenses + $amount_spent),
-            'harvests' => $harvests
+            'total_harvest' => intval($totalHarvest),
+            'total_capital' => $inventories + $totalExpenses + $amount_spent,
+            'total_profit' =>  $totalHarvest - ($inventories + $totalExpenses + $amount_spent),
+            'expenses' => $totalExpenses,
+            'harvests' => $harvests,
+
         ];
 
         return $this->success(
@@ -66,10 +86,9 @@ class HarvestController extends Controller
             data: $data,
             code: HttpStatusCode::SUCCESSFUL->value
         );
-
     }
 
-    public function show(Farm $farm, Harvest $harvest) : JsonResponse
+    public function show(Farm $farm, Harvest $harvest): JsonResponse
     {
         $harvest = $farm->harvests()->find($harvest->id);
 
@@ -87,7 +106,7 @@ class HarvestController extends Controller
         );
     }
 
-    public function store(createHarvestRequest $request, Farm $farm) : JsonResponse
+    public function store(createHarvestRequest $request, Farm $farm): JsonResponse
     {
         $harvest = $farm->harvests()->create($request->validated());
 
@@ -104,7 +123,7 @@ class HarvestController extends Controller
         );
     }
 
-    public function update(UpdateHarvestRequest $request,Farm $farm, Harvest $harvest) : JsonResponse
+    public function update(UpdateHarvestRequest $request, Farm $farm, Harvest $harvest): JsonResponse
     {
 
         $harvest->update($request->validated());
@@ -115,8 +134,4 @@ class HarvestController extends Controller
             code: HttpStatusCode::SUCCESSFUL->value
         );
     }
-
-
-
-
 }
