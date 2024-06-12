312b10b5e02b38f6eea4b268b16d5a5f4d89b5e8
ikeogu <ikeogu322@gmail.com>
2024-06-12 14:51:38 +0100

    added paystack keys

--- config/services.php
+++ config/services.php
@@ -32,8 +32,8 @@ return [
     ],
     'paystack' => [
         'base_url' => 'https://api.paystack.co',
-        'secret' => 'sk_test_ec55605903a80e44ff79774f80aa61db58edab53',
-        'public' => 'pk_test_7c3ebc26048b32d7c951ac4ba07a529f1ad99fc6'
+        'secret' => env('PAYSTACK_SECRET','sk_test_ec55605903a80e44ff79774f80aa61db58edab53'),
+        'public' => env('PAYSTACK_PUBLIC','pk_test_7c3ebc26048b32d7c951ac4ba07a529f1ad99fc6')
     ]
 
 ];
