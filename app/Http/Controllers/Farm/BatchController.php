1dcf35dd99f6ea102ee4af07d1d5ef822f48f74c
ikeogu <ikeogu322@gmail.com>
2024-12-27 12:45:47 +0100

    fixknf

--- app/Http/Controllers/Farm/BatchController.php
+++ app/Http/Controllers/Farm/BatchController.php
@@ -117,7 +117,7 @@ class BatchController extends Controller
         $batch->update($request->validated());
 
         if($batch->status === Status::SOLDOUT->value){
-            $batch->inventories->update(['status' => Status::SOLDOUT->value]);
+            $batch->inventories()->update(['status' => Status::SOLDOUT->value]);
         }
 
         return $this->success(
