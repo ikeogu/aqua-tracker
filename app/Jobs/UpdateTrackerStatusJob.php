3033a21cbd6fd509734259d09a362e5623988141
ikeogu <ikeogu322@gmail.com>
2024-12-27 12:16:43 +0100

    fixknf

--- app/Jobs/UpdateTrackerStatusJob.php
+++ app/Jobs/UpdateTrackerStatusJob.php
@@ -2,16 +2,13 @@
 
 namespace App\Jobs;
 
-use App\Enums\Status;
-use App\Models\Task;
-use App\Notifications\TaskNotification;
+use App\Services\TaskService;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
-use Carbon\Carbon;
 
 class UpdateTrackerStatusJob implements ShouldQueue
 {
@@ -28,46 +25,10 @@ class UpdateTrackerStatusJob implements ShouldQueue
     /**
      * Execute the job.
      */
-    public function handle(): void
+    public function handle(TaskService $taskService): void
     {
+        $taskService->executeTask();
 
-        $tasks = Task::where('status', Status::PENDING->value)->get();
-
-        $tasks->each(function ($task) {
-            $dueDate = Carbon::parse($task->due_date);
-            $startDate = Carbon::parse($task->start_date);
-            $now = now();
-
-            // Check if the task is overdue
-            if ($dueDate < $now) {
-                Log::debug(":::::::: task overdue");
-                $task->update(['status' => Status::OVERDUE->value]);
-                $task->farm->owner->notify(new TaskNotification($task, Status::OVERDUE->value));
-
-                // Handle repeating tasks
-                if ($task->repeat) {
-                    Log::info(":::::::: task not due");
-                    $task->update(['status' => Status::PENDING->value]);
-                }
-            } elseif ($startDate->isSameDay($now)) {
-                Log::info(":::::::: task due now");
-                $task->update(['status' => Status::ACTIVE->value]);
-                $task->farm->owner->notify(new TaskNotification($task, Status::ACTIVE->value));
-            } /* elseif ($dueDate > $now) {
-                Log::info(":::::::: task due in the future");
-                $task->update(['status' => Status::DUE->value]);
-                $task->farm->owner->notify(new TaskNotification($task, Status::DUE->value));
-            } */
-
-            // Notification reminders
-            $reminderTimes = ['1 day', '3 days', '7 days', '14 days', '30 days', '1 hr', '30 mins', '15 mins', '5 mins', '1 min'];
-            Log::info(":::::::: task reminder");
-            foreach ($reminderTimes as $reminderTime) {
-                Log::debug($reminderTime);
-                if ($dueDate->copy()->sub($reminderTime)->eq($now)) {
-                    $task->farm->owner->notify(new TaskNotification($task, 'pending', $reminderTime));
-                }
-            }
-        });
+        Log::debug(['logged for task']);
     }
 }
---
+++ app/Services/TaskService.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Services;
+
+use App\Enums\Status;
+use App\Models\Task;
+use App\Notifications\TaskNotification;
+use Illuminate\Support\Facades\Log;
+use Carbon\Carbon;
+
+
+class TaskService
+{
+
+    public function executeTask()
+    {
+        $tasks = Task::where('status', Status::PENDING->value)->get();
+
+        foreach ($tasks as $task) {
+            $dueDate = Carbon::parse($task->due_date);
+            $startDate = Carbon::parse($task->start_date);
+            $now = now();
+
+            // Check if the task is overdue
+            if ($dueDate->isPast()) {
+                Log::debug(":::::::: task overdue");
+                $task->update(['status' => Status::OVERDUE->value]);
+                $task->farm->owner->notify(new TaskNotification($task, Status::OVERDUE->value));
+
+                // Handle repeating tasks
+                if ($task->repeat) {
+                    Log::info(":::::::: task not due");
+                    $task->update(['status' => Status::PENDING->value]);
+                }
+            } elseif ($startDate->isToday()) {
+                Log::info(":::::::: task due now");
+                $task->update(['status' => Status::ACTIVE->value]);
+                $task->farm->owner->notify(new TaskNotification($task, Status::ACTIVE->value));
+            }
+            // Notification reminders
+            $reminderTimes = ['1 day', '3 days', '7 days', '14 days', '30 days', '1 hr', '30 mins', '15 mins', '5 mins', '1 min'];
+            Log::info(":::::::: task reminder");
+            foreach ($reminderTimes as $reminderTime) {
+                $reminderDate = $dueDate->copy()->sub($reminderTime);
+                if ($reminderDate->eq($now)) {
+                    Log::debug("Reminder for {$reminderTime} before due date: {$task->id}");
+                    $task->farm->owner->notify(new TaskNotification($task, 'pending', $reminderTime));
+                }
+            }
+        }
+    }
+}
