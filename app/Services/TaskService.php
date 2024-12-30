9f10f3c8935de147bc75c4b2793fbe5a1d9804f4
ikeogu <ikeogu322@gmail.com>
2024-12-30 09:32:05 +0100

    chores: admin referral pro

--- app/Services/TaskService.php
+++ app/Services/TaskService.php
@@ -7,7 +7,7 @@ use App\Models\Task;
 use App\Notifications\TaskNotification;
 use Illuminate\Support\Facades\Log;
 use Carbon\Carbon;
-
+use DateTime;
 
 class TaskService
 {
@@ -46,15 +46,34 @@ class TaskService
                 $task->farm->owner->notify(new TaskNotification($task, Status::ACTIVE->value));
             }
             // Notification reminders
-            $reminderTimes = ['1 day', '3 days', '7 days', '14 days', '30 days', '1 hour', '30 minutes', '15 minutes', '5 minutes', '1 minute'];
+            $durations = ['1 day', '3 days', '7 days', '14 days', '30 days', '1 hour', '30 minutes', '15 minutes', '5 minutes', '1 minute'];
+            $referenceTime = Carbon::parse($task->start_time);
             Log::info(":::::::: task reminder");
-            foreach ($reminderTimes as $reminderTime) {
-                $reminderDate = $dueDate->copy()->sub($reminderTime);
-                if ($reminderDate->eq($now)) {
-                    Log::debug("Reminder for {$reminderTime} before due date: {$task->id}");
-                    $task->farm->owner->notify(new TaskNotification($task, 'pending', $reminderTime));
+            foreach ($durations as $duration) {
+               // $reminderDate = $dueDate->copy()->sub($reminderTime);
+                $targetTime = $referenceTime->copy()->add($this->parseDurationToCarbonInterval($duration));
+                if ($now->equalTo($targetTime)) {
+                    Log::debug("Reminder for {$targetTime} before due date: {$task->id}");
+                    $task->farm->owner->notify(new TaskNotification($task, 'pending', $targetTime));
                 }
             }
         }
     }
+
+        /**
+         * Converts a duration string like "1 day" or "30 minutes" into a Carbon interval.
+         *
+         * @param string $duration
+         * @return \DateInterval
+         */
+        function parseDurationToCarbonInterval(string $duration): \DateInterval
+        {
+            // Use the 'modify' method of DateTime to parse natural language time expressions
+            $date = new DateTime();
+            $date->modify($duration);
+            $now = new DateTime();
+
+            // Calculate the difference and return as DateInterval
+            return $now->diff($date);
+        }
 }
